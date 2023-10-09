64cb5be72aa9ff3b023916f369de88890684e996
Omar Elnemr <omar.elnemr@nagwa.com>
2023-10-09 15:43:19 +0300

    added pull down button.

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,12 +1,15 @@
+import 'package:background_downloader/background_downloader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:not_lame_downloader/app_assets.dart';
+import 'package:not_lame_downloader/cubits/download_cubit/download_cubit.dart';
 import 'package:not_lame_downloader/helpers/extensions/context_extension.dart';
 import 'package:not_lame_downloader/screens/download_task_page.dart';
 import 'package:not_lame_downloader/screens/widgets/modal_popups/add_download_modal_popup.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:pull_down_button/pull_down_button.dart';
 
 import 'cubits/theme_cubit/theme_cubit.dart';
 
@@ -17,34 +20,71 @@ class MainPage extends HookWidget {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        leading: BlocBuilder<ThemeCubit, ThemeMode>(builder: (context, state) {
-          return CupertinoButton(
-            padding: EdgeInsets.zero,
-            child: Icon(state == ThemeMode.system
-                ? Icons.brightness_4
-                : state == ThemeMode.light
-                    ? CupertinoIcons.brightness_solid
-                    : CupertinoIcons.moon_fill),
-            onPressed: () {
-              final ThemeMode newThemeMode = (state == ThemeMode.system)
-                  ? ThemeMode.dark
-                  : state == ThemeMode.dark
-                      ? ThemeMode.light
-                      : ThemeMode.system;
-              ThemeCubit.get(context).changeBrightness(newThemeMode);
-            },
-          );
-        }),
-        middle: const Text('NotLameDownloader'),
-        trailing: CupertinoButton(
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              showCupertinoModalPopup(
-                  context: context,
-                  builder: (context) => const AddDownloadModalPopUp());
-            },
-            child: const Icon(CupertinoIcons.add)),
-      ),
+          leading: BlocBuilder<ThemeCubit, ThemeMode>(builder: (context, state) {
+            return CupertinoButton(
+              padding: EdgeInsets.zero,
+              child: Icon(state == ThemeMode.system
+                  ? Icons.brightness_4
+                  : state == ThemeMode.light
+                      ? CupertinoIcons.brightness_solid
+                      : CupertinoIcons.moon_fill),
+              onPressed: () {
+                final ThemeMode newThemeMode = (state == ThemeMode.system)
+                    ? ThemeMode.dark
+                    : state == ThemeMode.dark
+                        ? ThemeMode.light
+                        : ThemeMode.system;
+                ThemeCubit.get(context).changeBrightness(newThemeMode);
+              },
+            );
+          }),
+          middle: const Text('NotLameDownloader'),
+          trailing: PullDownButton(
+            itemBuilder: (context) => [
+              PullDownMenuItem(
+                title: 'Add Download',
+                onTap: () {
+                  showCupertinoModalPopup(context: context, builder: (context) => const AddDownloadModalPopUp());
+                },
+              ),
+              PullDownMenuItem(
+                title: 'Delete All Tasks',
+                onTap: () {
+                  showCupertinoDialog(
+                      context: context,
+                      builder: (context) => CupertinoAlertDialog(
+                            title: const Text('Delete All Tasks!'),
+                            content: const Text('Are you sure you want to delete all tasks?'),
+                            actions: [
+                              CupertinoDialogAction(
+                                onPressed: () =>
+                                  FileDownloader().database.deleteAllRecords().then((value) {
+                    FileDownloader().destroy(); DownloadCubit
+                        .get(context)
+                        .getDownloadedTasks()
+                        .then((value) => Navigator.of(context).pop());}),
+                                isDestructiveAction: true,
+                                child: const Text(
+                                  'Yes',
+                                )
+                              ),
+                              CupertinoDialogAction(
+                                child: const Text(
+                                  'No',
+                                ),
+                                onPressed: () =>Navigator.of(context).pop(),
+                              ),
+                            ],
+                          ));
+                },
+              ),
+            ],
+            buttonBuilder: (context, showMenu) => CupertinoButton(
+              onPressed: showMenu,
+              padding: EdgeInsets.zero,
+              child: const Icon(CupertinoIcons.ellipsis_circle),
+            ),
+          )),
       child: CupertinoTabScaffold(
         tabBar: CupertinoTabBar(items: const [
           BottomNavigationBarItem(
@@ -52,8 +92,7 @@ class MainPage extends HookWidget {
                 CupertinoIcons.down_arrow,
               ),
               label: 'Downloads'),
-          BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.archivebox_fill), label: 'Files')
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.archivebox_fill), label: 'Files')
         ]),
         tabBuilder: (context, index) {
           return Stack(
@@ -64,12 +103,8 @@ class MainPage extends HookWidget {
                   fit: BoxFit.contain,
                   height: context.height * .2,
                   width: context.width * .2,
-                  theme: SvgTheme(
-                      currentColor:
-                          CupertinoTheme.of(context).barBackgroundColor),
-                  colorFilter: ColorFilter.mode(
-                      CupertinoTheme.of(context).barBackgroundColor,
-                      BlendMode.hardLight),
+                  theme: SvgTheme(currentColor: CupertinoTheme.of(context).barBackgroundColor),
+                  colorFilter: ColorFilter.mode(CupertinoTheme.of(context).barBackgroundColor, BlendMode.hardLight),
                 ),
               ),
               const DownloadTasksPage(),
